35bc7e3a88807de0cb127f39b80974415dddb613
dor3518@gmail.com <dor3518@gmail.com>
2023-05-16 18:58:23 +0300

    DN

--- sources/Character.cpp
+++ sources/Character.cpp
@@ -2,7 +2,7 @@
 
 Character::Character(string name, const Point &location, int hitpoints) : name(name), location(location), hitpoints(hitpoints) {}
 bool Character::isAlive() { return false; }
-double Character::distance(const Character &other) { return 0; }
+double Character::distance(const Character *other) { return 0; }
 void Character::hit(int dmg){};
 string Character::getName() { return ""; }
 Point Character::getLocation() { return Point(0, 0); }
